22cb29c45b2a8325dbf86761112f4b6c36ad90b9
Corie Beale <bealecorie@gmail.com>
2024-11-11 19:56:21 -0500

    refactored to start

--- server/app.js
+++ server/app.js
@@ -2,42 +2,39 @@ const path = require('path');
 const express = require('express');
 const compression = require('compression');
 const favicon = require('serve-favicon');
-const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser'); // TODO: remove
 const mongoose = require('mongoose');
 const expressHandlebars = require('express-handlebars');
+const helmet = require('helmet');
+
+const router = require('./router.js');
 
 const port = process.env.PORT || process.env.NODE_PORT || 3000;
 
-const dbURL = process.env.MONGODB_URI || 'mongodb://localhost/DomoMaker';
+const dbURI = process.env.MONGODB_URI || 'mongodb+srv://dbAdmin:cfb3749Password@richmedia.1a1ll.mongodb.net/DomoMaker';
 
-const mongooseOptions = {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-};
-
-mongoose.connect(dbURL, mongooseOptions, (err) => {
+mongoose.connect(dbURI).catch((err) => {
   if (err) {
-    console.log('Could not connect to DB');
+    console.log('Could not connect to database');
     throw err;
   }
 });
 
-const router = require('./router.js');
-
 const app = express();
+
+app.use(helmet());
 app.use('/assets', express.static(path.resolve(`${__dirname}/../hosted/`)));
 app.use(favicon(`${__dirname}/../hosted/img/favicon.png`));
 app.use(compression());
+app.use(bodyParser.urlencoded({ extended: true }));//TODO: remove
+app.use(bodyParser.json());//TODO: remove
 app.engine('handlebars', expressHandlebars({ defaultLayout: 'main' }));
 app.set('view engine', 'handlebars');
 app.set('views', `${__dirname}/../views`);
-app.use(cookieParser());
 
 router(app);
 
 app.listen(port, (err) => {
-  if (err) {
-    throw err;
-  }
-  console.log(`listening on port ${port}`);
+  if (err) { throw err; }
+  console.log(`Listening on port ${port}`);
 });
